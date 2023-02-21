0b998458ce29c00254bbac6004131db15c80530f
erguotou <erguotou525@gmail.com>
2023-02-21 10:25:21 +0800

    fix: remove unnecessary for init

--- custom-ts/auth/postAuthentication.ts
+++
@@ -1,6 +0,0 @@
-
-import { AuthenticationHookRequest } from 'generated/fireboom.hooks'
-
-export default async function postAuthentication(hook: AuthenticationHookRequest) : Promise<void>{
-	// TODO: 在此处添加代码
-}
